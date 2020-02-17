5c7baf69a55d2cbf7ad1a2ec3de5e3ac93e15dd1
Evert Timberg <evert.timberg+github@gmail.com>
2020-02-17 16:38:39 -0500

    Net error strategy implementation

--- pycircuitbreaker/strategies/net_error.py
+++ pycircuitbreaker/strategies/net_error.py
@@ -3,39 +3,36 @@ from datetime import datetime
 from ..state import CircuitBreakerState
 
 
-class SingleResetStrategy:
+class NetErrorStrategy:
     def __init__(self, error_threshold, recovery_threshold):
-        self._error_count = 0
         self._error_threshold = error_threshold
+        self._net_error_count = 0
         self._recovery_threshold = recovery_threshold
-        self._success_count = 0
         self._state = CircuitBreakerState.CLOSED
 
     def handle_error(self) -> bool:
-        self._error_count += 1
+        self._net_error_count += 1
         opened = False
 
-        if self._error_count >= self._error_threshold:
+        if self._net_error_count >= self._error_threshold:
             self._state = CircuitBreakerState.OPEN
-            self._success_count = 0
             opened = True
 
         return opened
 
     def handle_success(self) -> bool:
-        self._success_count += 1
+        self._net_error_count = max(0, self._net_error_count - 1)
         closed = False
 
-        if self._success_count >= self._recovery_threshold:
+        if self._net_error_count < self._error_threshold:
             self._state = CircuitBreakerState.CLOSED
-            self._error_count = 0
             closed = True
 
         return closed
 
     @property
     def error_count(self) -> int:
-        return self._error_count
+        return max(0, self._net_error_count - self._error_threshold)
 
     @property
     def state(self) -> CircuitBreakerState:
@@ -43,4 +40,4 @@ class SingleResetStrategy:
 
     @property
     def success_count(self) -> int:
-        return self._success_count
+        return max(0, self._error_threshold - self._net_error_count)
